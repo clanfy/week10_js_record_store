d4373d7104211f1ad429810261cfd5fe9677d7b9
Clacson <cookiellacson@gmail.com>
2016-12-02 11:36:35 +0000

    create 3 new records, tests edited to test those records

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -4,9 +4,15 @@ var Record = require( '../record' );
 describe( "Record", function(){
 
   var mariahRecord = null;
+  var beachBoysRecord = null;
+  var frankRecord = null;
+  var bieberRecord = null;
 
   beforeEach( function(){
     mariahRecord = new Record( "Mariah Carey", "Merry Christmas", 10.00 );
+    beachBoysRecord = new Record( "The Beach Boys", "The Beach Boys Christmas Album", 12.50);
+    frankRecord = new Record( "Frank Sinatra", "A Jolly Christmas From Frank Sinatra", 9.00);
+    bieberRecord = new Record( "Justin Bieber", "Under The Mistletoe", 6.66);
   });
 
   it( "should have artist passed in constructor", function(){
@@ -14,11 +20,11 @@ describe( "Record", function(){
   });
 
   it( "should have title passed in constructor", function(){
-    assert.equal( "Merry Christmas", mariahRecord.title );
+    assert.equal( "The Beach Boys Christmas Album", beachBoysRecord.title );
   });
 
   it( "should have price passed in constructor", function(){
-    assert.equal( 10.00, mariahRecord.price );
+    assert.equal( 6.66, bieberRecord.price );
   });
 
 });
