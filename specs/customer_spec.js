63646a175e21e3e136a8b7ae9f7fc68a848b0d74
Clacson <cookiellacson@gmail.com>
2016-12-02 15:36:00 +0000

    passes 18 tests, customer wallet/money is passed to constructor

--- specs/customer_spec.js
+++ specs/customer_spec.js
@@ -9,8 +9,12 @@ describe( "Customer", function(){
     santa = new Customer("Santa Claus", 100.00);
   });
 
-  it( "should have customer named passed to constructor", function(){
+  it( "should have customer name passed to constructor", function(){
     assert.equal( "Santa Claus", santa.name );
+  });
+
+  it( "should have customer wallet passed to constructor", function(){
+    assert.equal( 100.00, santa.wallet );
   })
 
 });
